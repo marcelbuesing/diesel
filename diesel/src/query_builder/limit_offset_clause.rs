d0d6d4b6e5315bbcf96f2b3e5308cdaf5bd733a1
Georg Semmler <georg_semmler_05@web.de>
2019-10-10 15:47:04 +0200

    Make Limit/Offset clauses public, because custom backends need to implement stuff here

--- diesel/src/query_builder/limit_offset_clause.rs
+++ diesel/src/query_builder/limit_offset_clause.rs
@@ -1,14 +1,23 @@
 use super::QueryFragment;
 
-#[doc(hidden)]
+/// A helper query node that contains both limit and offset clauses
+///
+/// This type is only relevant for implementing custom backends
 #[derive(Debug, Clone, Copy, QueryId)]
 pub struct LimitOffsetClause<Limit, Offset> {
-    pub(crate) limit_clause: Limit,
-    pub(crate) offset_clause: Offset,
+    /// The limit clause
+    pub limit_clause: Limit,
+    /// The offset clause
+    pub offset_clause: Offset,
 }
 
+/// A boxed variant of [`LimitOffsetClause`](../struct.LimitOffsetClause.html)
+///
+/// This type is only relevant for implementing custom backends
 #[allow(missing_debug_implementations)]
 pub struct BoxedLimitOffsetClause<'a, DB> {
-    pub(crate) limit: Option<Box<dyn QueryFragment<DB> + 'a>>,
-    pub(crate) offset: Option<Box<dyn QueryFragment<DB> + 'a>>,
+    /// The limit clause
+    pub limit: Option<Box<dyn QueryFragment<DB> + 'a>>,
+    /// The offset clause
+    pub offset: Option<Box<dyn QueryFragment<DB> + 'a>>,
 }
